53e27795b029803639e78510079476012fafb142
Henok <henokassefa494@gmail.com>
2025-03-30 21:48:01 -0400

    Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -17,4 +17,24 @@ function fetchProductsThen() {
         console.log('Error fetching products:', error.message);
       });
   }
+
+  // Task 3: Fetch products using async/await and try/catch
+async function fetchProductsAsync() {
+    try {
+      const response = await fetch('https://www.course-api.com/javascript-store-products');
   
+      if (!response.ok) {
+        throw new Error(`Network response was not ok (${response.status})`);
+      }
+  
+      const products = await response.json();
+  
+      // Call the function to display products on the page (to be created in Task 4)
+      displayProducts(products);
+      
+    } catch (error) {
+      // Pass any errors to handleError (to be created in Task 5)
+      handleError(error);
+    }
+}
+
